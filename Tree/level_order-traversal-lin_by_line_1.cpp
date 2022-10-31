5daf9bc46341a042bd1e33c046b504d753171a54
Maneel Chauhan <109906931+code12repeat@users.noreply.github.com>
2022-11-01 00:20:25 +0530

    Add files via upload

---
+++ Tree/level_order-traversal-lin_by_line_1.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int key;
+    Node*left;
+    Node*right;
+    Node(int x){
+     key=x;
+     left=right=NULL;
+    }
+
+};
+void LevelTraversal(Node *root){
+    queue<Node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(q.size()>1){
+    Node *curr=q.front();
+    q.pop();
+    if(curr==NULL){
+        cout<<"\n";
+        q.push(NULL);
+        continue;
+    }
+    cout<<curr->key<<" ";
+    if(curr->left!=NULL)
+    q.push(curr->left);
+    if(curr->right!=NULL)
+    q.push(curr->right);
+    }
+}
+int main(){
+    
+   Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	root->left->right=new Node(50);
+	root->right->left=new Node(60);
+	root->right->right=new Node(70);
+
+	LevelTraversal(root);
+}
